97aaef0d2c781af3981a6801136c10e0a3f329c5
butschster <butschster@gmail.com>
2016-05-27 11:45:53 +0300

    Add new helpers

---
+++ src/Http/Form.php
@@ -0,0 +1,111 @@
+<?php
+
+namespace KodiComponents\Support\Http;
+
+use Illuminate\Foundation\Validation\ValidatesRequests;
+use Illuminate\Http\Request;
+
+abstract class Form
+{
+    use ValidatesRequests;
+
+    /**
+     * The request object.
+     *
+     * @var Request
+     */
+    protected $request;
+
+    /**
+     * Form constructor.
+     *
+     * @param Request|null $request
+     */
+    public function __construct(Request $request = null)
+    {
+        $this->request = $request ?: request();
+    }
+
+    /**
+     * Save and persist the form.
+     *
+     * @return bool
+     */
+    public function save()
+    {
+        if ($this->isValid()) {
+            $this->persist();
+
+            return true;
+        }
+
+        return false;
+    }
+
+    /**
+     * Fetch all fields from the form.
+     *
+     * @return array
+     */
+    public function fields()
+    {
+        return $this->request->all();
+    }
+
+    /**
+     * @return array
+     */
+    public function rules()
+    {
+        return [];
+    }
+
+    /**
+     * @return array
+     */
+    public function labels()
+    {
+        return [];
+    }
+
+    /**
+     * @return Request
+     */
+    public function request()
+    {
+        return $this->request;
+    }
+
+    /**
+     * Persist the form.
+     *
+     * @return mixed
+     */
+    abstract public function persist();
+
+    /**
+     * Determine if the form is valid.
+     *
+     * @return bool
+     */
+    public function isValid()
+    {
+        $this->validate($this->request, $this->rules(), $this->labels());
+
+        return true;
+    }
+
+    /**
+     * Fetch properties from the request.
+     *
+     * @param string $property
+     *
+     * @return array|string
+     */
+    public function __get($property)
+    {
+        // Consider doing a bit more checking here...
+
+        return $this->request->input($property);
+    }
+}
